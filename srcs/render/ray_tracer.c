ef4828b7b0cf87fd671cae8f0b1060a85ee3b563
jaguillo <juloo.dsi@gmail.com>
2015-03-20 19:31:27 +0100

    ...

--- srcs/render/ray_tracer.c
+++ srcs/render/ray_tracer.c
@@ -1,31 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   draw_scene.c                                       :+:      :+:    :+:   */
+/*   ray_tracer.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/03/13 19:54:21 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/03/20 19:26:19 by jaguillo         ###   ########.fr       */
+/*   Created: 2015/03/20 18:15:59 by jaguillo          #+#    #+#             */
+/*   Updated: 2015/03/20 18:18:39 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
-#include "msg.h"
 
-void			draw_scene(t_render *r, t_scene *scene)
+t_color			ray_trace(t_render *r, )
 {
-	t_pt			pt;
-
-	r->scene = scene;
-	ft_printf(RENDER_SCENE_MSG, scene->name);
-	pt = PT(-1, -1);
-	while (++pt.y < r->img.height)
-	{
-		pt.x = -1;
-		while (++pt.x < r->img.width)
-		{
-			ft_drawpt(&(r->img), pt, ray_trace(r, ))
-		}
-	}
 }
